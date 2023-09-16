3e9e773d77378009ca8c9c179e14bdb9d4ff53ea
tushar <Tushar.chikane@cloverinfotech.com>
2023-09-16 13:05:35 +0530

    initial commit

---
+++ src/main/java/com/example/LindaSchool/Loginservice/Loginservice.java
@@ -0,0 +1,20 @@
+package com.example.LindaSchool.Loginservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.LindaSchool.Logindomain.Login;
+import com.example.LindaSchool.Loginreposotory.LoginRepository;
+
+@Service
+public class Loginservice {
+	
+	@Autowired
+	private LoginRepository repo;
+	  
+	  public Login login(String username, String password) {
+	  Login user = repo.findByUsernameAndPassword(username, password);
+	   return user;
+	  }
+
+}
